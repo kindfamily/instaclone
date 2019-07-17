ee0c19268fe6cb3376123425f2a33c26a2c16133
bill <tuntunkimpo@naver.com>
2019-07-17 14:36:17 +0000

    venv 폴더도 추가

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instaclone',
+        'USER': 'bill',
+        'PASSWORD': '123456789',
+        'HOST':'localhost',
+        'PORT': '',
     }
 }
 
